638b1c15d8d39a6db690cd3e9c5510e1e2fe53e1
Praveena Osuri <praveenaosuri@gmail.com>
2026-04-19 21:22:06 +0530

    Day 3 -  Registration API working with BCrypt

--- src/main/java/com/jobtracker/job_tracker_backend/controller/AuthController.java
+++ src/main/java/com/jobtracker/job_tracker_backend/controller/AuthController.java
@@ -1,10 +1,22 @@
 package com.jobtracker.job_tracker_backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jobtracker.job_tracker_backend.entity.User;
+import com.jobtracker.job_tracker_backend.service.UserService;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    
+    @Autowired
+    private UserService userService;
+ 
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user){
+        return userService.registerUser(user);
+    }
 }
