509b2e1f3adcbf4be60d57ba1a89ed1f917aa9ab
Praveena Osuri <praveenaosuri@gmail.com>
2026-04-27 22:38:03 +0530

    Added securityFilter

--- src/main/java/com/jobtracker/job_tracker_backend/config/SecurityConfig.java
+++ src/main/java/com/jobtracker/job_tracker_backend/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.jobtracker.job_tracker_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -13,6 +15,15 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();        
     }
     
+    @Bean
+    public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception{
+        http.
+         csrf(csrf -> csrf.disable())
+         .authorizeHttpRequests(requests -> requests
+          .requestMatchers("/api/auth/**").permitAll()
+          .anyRequest().authenticated());
+        return http.build();
+    }
     
 
 }
