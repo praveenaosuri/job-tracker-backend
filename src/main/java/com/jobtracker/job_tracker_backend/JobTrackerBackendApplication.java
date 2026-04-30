b8d9e36460d0303b4b484343b16938777fd0e4f8
Praveena Osuri <praveenaosuri@gmail.com>
2026-04-30 22:25:21 +0530

    Removed exclude in security

--- src/main/java/com/jobtracker/job_tracker_backend/JobTrackerBackendApplication.java
+++ src/main/java/com/jobtracker/job_tracker_backend/JobTrackerBackendApplication.java
@@ -2,11 +2,7 @@ package com.jobtracker.job_tracker_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication(
-    exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-    }
-)
+@SpringBootApplication
 public class JobTrackerBackendApplication {
 
 	public static void main(String[] args) {
